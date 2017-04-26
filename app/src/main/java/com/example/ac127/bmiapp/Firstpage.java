341c8175cbd9134c7fcd29b3ad00d48768518556
gatune <supapich.thit@bumail.net>
2017-04-26 19:01:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ac127/bmiapp/Firstpage.java
@@ -0,0 +1,14 @@
+package com.example.ac127.bmiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Firstpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firstpage);
+    }
+
+}
